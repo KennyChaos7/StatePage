04d72ae6ef7469c08b4e630f51e2750efd035ca8
kenny <kennychaos7@gmail.com>
2021-01-25 17:52:47 +0800

    1. 架构编写0.2 2. 基本show/bind功能完成
    TODO
    监听功能

--- statepage/src/main/java/com/statepage/simple/ReloadingStatePage.kt
+++ statepage/src/main/java/com/statepage/simple/ReloadingStatePage.kt
@@ -1,6 +1,7 @@
 package com.statepage.simple
 
+import android.content.Context
 import com.statepage.AbsStatePage
+import com.statepage.R
 
-class ReloadingStatePage: AbsStatePage() {
-}
+class ReloadingStatePage(context: Context) : AbsStatePage(context, R.layout.layout_reloading_statepage)
