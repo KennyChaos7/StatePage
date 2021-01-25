ce8b66ce206cc2d19fc2747ffd99f0017e2cef66
kenny <kennychaos7@gmail.com>
2021-01-25 17:52:47 +0800

    1. 架构编写0.2 2. 基本show/bind功能完成
    TODO
    监听功能

--- app/src/main/java/com/example/SecondFragment.kt
+++ app/src/main/java/com/example/SecondFragment.kt
@@ -1,31 +1,45 @@
 package com.example
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
+import com.statepage.StatePageManager
+import com.statepage.simple.ReloadingStatePage
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class SecondFragment : Fragment() {
 
+    private lateinit var statePage: ReloadingStatePage
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        val root: View = inflater.inflate(R.layout.fragment_second, container, false)
+        statePage = ReloadingStatePage(requireContext())
+
+        StatePageManager.bind(root, statePage)
+
+        return root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+//            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+            StatePageManager.show<ReloadingStatePage>(statePage)
         }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        StatePageManager.unbind<ReloadingStatePage>(statePage)
     }
 }
