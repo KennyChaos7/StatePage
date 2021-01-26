6809dad2911f33081ebd6a5bae06e15bf1e05d2a
kenny <kennychaos7@gmail.com>
2021-01-26 19:26:23 +0800

    添加默认statePage中...

--- statepage/src/main/java/com/statepage/simple/ReloadingStatePage.kt
+++ statepage/src/main/java/com/statepage/simple/ReloadingStatePage.kt
@@ -4,4 +4,9 @@ import android.content.Context
 import com.statepage.AbsStatePage
 import com.statepage.R
 
-class ReloadingStatePage(context: Context) : AbsStatePage(context, R.layout.layout_reloading_statepage)
+class ReloadingStatePage(context: Context, listener: () -> Unit) : AbsStatePage(context, R.layout.layout_reloading_statepage)
+{
+    init {
+        findView(R.id.ivReload).setOnClickListener { listener.invoke() }
+    }
+}
