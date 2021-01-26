9ce8a03af957023aa4532bfaa8468ca6cc829214
kenny <kennychaos7@gmail.com>
2021-01-26 15:19:07 +0800

    1. 修改了show/bind功能, 不再单纯使用VISIBLE/GONE, 直接addView/removeView 2. 下版本更新监听功能和部分默认StatePage

--- app/src/main/java/com/example/MainActivity.kt
+++ app/src/main/java/com/example/MainActivity.kt
@@ -1,23 +1,45 @@
 package com.example
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Handler
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.statepage.StatePageManager
+import com.statepage.simple.ReloadingStatePage
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var statePage: ReloadingStatePage
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                    .setAction("Action", null).show()
+            StatePageManager.show<ReloadingStatePage>(statePage)
+
+            Handler().postDelayed(
+                    Runnable {
+                        StatePageManager.dismiss<ReloadingStatePage>(statePage)
+                    }, 5000
+            )
         }
+        val content_main = findViewById<View>(R.id.content_main)
+        val linearLayout = findViewById<View>(R.id.linearLayout)
+
+        statePage = ReloadingStatePage(this)
+
+//        StatePageManager.bind(this, statePage)
+        StatePageManager.bind(content_main, statePage)
+//        StatePageManager.bind(fab, statePage)
+//        StatePageManager.bind(linearLayout, statePage)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- statepage/src/main/java/com/statepage/StatePageBinder.kt
+++ statepage/src/main/java/com/statepage/StatePageBinder.kt
@@ -1,7 +1,17 @@
 package com.statepage
 
+import android.view.View
+import android.view.ViewGroup
+
 data class StatePageBinder(
         val contextName: String,
         val bindName: String,
+        val targetView: View,
+        val targetViewParent: ViewGroup,
         val absStatePage: AbsStatePage
 )
+{
+    override fun toString(): String {
+        return "StatePageBinder(contextName='$contextName', bindName='$bindName', targetView=$targetView, absStatePage=$absStatePage)"
+    }
+}
