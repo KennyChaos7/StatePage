ba86215028795f8ae11cecac10bf9fc0e8d71c73
ouniankai <ouniankai>
2021-01-30 13:55:40 +0800

    1. 更新了文档 2. 优化了StatePageManager的逻辑

---
+++ statepage/src/main/java/com/statepage/simple/NetworkErrorStatePage.kt
@@ -0,0 +1,7 @@
+package com.statepage.simple
+
+import android.content.Context
+import com.statepage.AbsStatePage
+import com.statepage.R
+
+class NetworkErrorStatePage(context: Context) : AbsStatePage(context, R.layout.layout_network_error_statepage)
